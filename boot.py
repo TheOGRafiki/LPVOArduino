55f0a6d2230070bf433b3897c3a2dbbfb3e507d9
TheOGRafiki <ahmedbusaidi82@gmail.com>
2021-12-24 18:02:54 -0500

    Prelim Updates to file struct

--- boot.py
+++ boot.py
@@ -1,12 +1,34 @@
-import pyb # Import module for board related functions
+from machine import I2C
+
 
 # TEST CONNECTIONS
+def generate_report(append_df):
+    return append_df
+
+def generate_error_report(append_df):
+    return append_df
+
 def init_i2c():
+
+    # Create I2C Controller 
+    # Find all Connections
+    # Test All 
+    # IF FAIL Log it
+    xSCL = 'PH7'
+    xSDA = 'PH8'
+    xFreq = 100000
+
+    try: 
+        i2c_controller = I2C(scl= xSCL, sda=xSDA, freq=xFreq)
+    except:
+        print("An Error Has Occured")
+    finally:
+        print("Fatal Error")
+    
 def init_uart():
+
 def init_adc():
+
 def init_ble():
+
 def init_connections():
-
-# Test load everything to max value
-def full_load():
-
