eaec20b6e9501ab69cd9380cd678b25e00341547
Ahmed Al Busaidi <36058005+TheOGRafiki@users.noreply.github.com>
2021-12-31 17:23:18 -0500

    ....

--- boot.py
+++ boot.py
@@ -1,12 +1,19 @@
-import pyb # Import module for board related functions
-
 # TEST CONNECTIONS
-def init_i2c():
-def init_uart():
-def init_adc():
-def init_ble():
-def init_connections():
+def init_i2c():{}
+def init_uart():{}
+def init_adc():{}
+def init_ble():{}
+def init_connections():{}
 
 # Test load everything to max value
-def full_load():
+def full_load():{}
 
+from machine import Pin
+from time import sleep
+
+led = Pin(2, Pin.OUT)
+
+while True:
+  led.value(not led.value())
+  sleep(0.5)
+# MATH FUNCTIONS
