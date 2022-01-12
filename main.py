c749a24f2b1b1eb609575e2d859e57d89c5e2428
TheOGRafiki <ahmedbusaidi82@gmail.com>
2022-01-11 22:56:31 -0500

    First Actual Update

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
-from pyb import I2C
 import logging
+from pyb import LED
+from time import sleep
+import micropython 
+from pyb import I2C
 
-class OLED:
-  def __init__(self):
-    try:
-        global i2c_con = I2C(1, I2C.CONTROLLER)
-    except:
-      # Load Json Error Message Here
-      logging.error("Random Error Message")
+def blink(status):
 
-  def write_data(string):
-    i2c_con.mem_write()
+  leb_obj = "Empty"
 
-  def reset():
-    i2c_con.mem_write()
+  if(status == True):
+    led_obj = pyb.LED(2) # Green Led
+  else: 
+    led_obj = pyb.LED(1) # Red Led
+
+  for x in range(3):
+    leb_obj.on()
+    sleep(1)
+    leb_obj.off()
+    sleep(1)  
+
+
+class dislay(self, i2c_obj):
+    i2c_obj = I2C(1)
+
+    def __init__(self, i2c_obj):
+      self.i2c_obj = i2c_obj
+      i2c_obj.init(I2C.CONTROLLER, baudrate="9600")
+
+
+
+    def write_command(i2c_obj, text):
+      i2c_obj.send(0x3C)
+      i2c_obj.send(0x00)
+      i2c_obj.send(text)
+
+    def write_text(i2c_obj. data):
+      i2c_obj.write(0x3C)
+      i2c_obj.write(0xC0)
+      i2c_obj.write(data)
+      
+
+
+
 
