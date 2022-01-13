c97c852049e94b2fdad0870082e264055461cad9
Ahmed Al Busaidi <36058005+TheOGRafiki@users.noreply.github.com>
2022-01-13 11:35:17 -0500

    Finishing up display class

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import logging
 from pyb import LED
 from time import sleep
 import micropython
@@ -20,62 +19,43 @@ def blink(status):
     sleep(1)
 
 
-class dislay(self, i2c_obj):
-    i2c_obj = I2C(1)
+class dislay(self):
+    i2c_obj = I2C(X)
+    devices = "Empty"
 
-    def __init__(self, i2c_obj):
+    def __init__(self):
+      blink(True)
+      try:
+        i2c_obj.init(I2C.CONTROLLER, baudrate="9600")
+        devices = i2c_obj.scan()
+        print(devices)
 
-      self.i2c_obj = i2c_obj
-      i2c_obj.init(I2C.CONTROLLER, baudrate="9600")
+      except Exception:
+        blink(False)
 
-      pyb.delay(100)
-      print("Running it down")
-      pyb.delay(120)
+    def write_command(text):
+      self.i2c_obj.send(0x3C)
+      self.i2c_obj.send(0x00)
+      self.i2c_obj.send(text)
 
-      write_command(0xFD)
-      write_command(0X12)
-      write_command(0XAE)
-      write_command(0xD5)
-      write_command(0X30)
-      write_command(0xA8)
-      write_command(0X37)
-      write_command(0xD3)
-      write_command(0X08)
-      write_command(0x40)
-      write_command(0xA1)
-      write_command(0xC8)
-      write_command(0xDA)
-      write_command(0x12)
-      write_command(0x81)
-      write_command(0x12)
-      write_command(0x8F)
-      write_command(0xD9)
-      write_command(0x25)
-      write_command(0xDB)
-      write_command(0x34)
-      write_command(0xA4)
-      write_command(0xA6)
+    def write_text(data):
+      self.i2c_obj.write(0x3C)
+      self.i2c_obj.write(0xC0)
+      self.i2c_obj.write(data)
+
+    def set_column_address(a, b):
+
+    def set_page_address(a, b):
+
+    def fill_ram(data):
 
 
+class range_finder():
+    def __init__(self, spi_obj):
+        self.spi_obj = spi_obj
+        spi_obj.init()
 
 
-
-
-
-
-
-    def write_command(i2c_obj, text):
-      i2c_obj.send(0x3C)
-      i2c_obj.send(0x00)
-      i2c_obj.send(text)
-
-    def write_text(i2c_obj. data):
-      i2c_obj.write(0x3C)
-      i2c_obj.write(0xC0)
-      i2c_obj.write(data)
-
-
-
-
-
+def __main__():
+    # main loop goes here
 
