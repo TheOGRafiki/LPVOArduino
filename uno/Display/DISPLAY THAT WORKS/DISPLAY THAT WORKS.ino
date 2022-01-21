03993e1facc102721665e0bbe7f39f172cb5ff72
Ahmed Al Busaidi <36058005+TheOGRafiki@users.noreply.github.com>
2022-01-20 21:42:30 -0500

    Added Button Support

--- uno/Display/DISPLAY THAT WORKS/DISPLAY THAT WORKS.ino
+++ uno/Display/DISPLAY THAT WORKS/DISPLAY THAT WORKS.ino
@@ -5,18 +5,32 @@
 /* Constructor */
 U8X8_SSD1306_128X64_NONAME_4W_SW_SPI u8x8(/* clock=*/ 13, /* data=*/ 11, /* cs=*/ 8, /* dc=*/ A0, /* reset=*/ 9);
 
+const int buttonPin = 2;
+int buttonState = 0;
 /* u8x8.begin() is required and will sent the setup/init sequence to the display */
 void setup(void)
 {
   u8x8.begin();
+
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT_PULLUP);
+  u8x8.setFont(u8x8_font_chroma48medium8_r);
+
+  
+
 }
 
 void loop(void)
-{
-  char* outputRows = "Rows: " + u8x8.getCols();
-  char* outputCols = "Cols: " + u8x8.getRows();
-  u8x8.setFont(u8x8_font_chroma48medium8_r);
-  u8x8.drawString(4,4,outputCols);
-  u8x8.drawString(0,0, outputRows);
-  delay(1000);
+  {
+    buttonState = digitalRead(buttonPin);
+  if(buttonState != HIGH) {
+    u8x8.drawString(1,3,"Button Pressed");
+    delay(200);
+
+  }
+  else {
+      u8x8.clearDisplay();
+  }
+
+  delay(1000); 
 }
