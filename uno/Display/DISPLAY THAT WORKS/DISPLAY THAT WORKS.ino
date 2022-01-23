61308348981a01d795c8f1f3274940bb5bf6448a
Ahmed Al Busaidi <36058005+TheOGRafiki@users.noreply.github.com>
2022-01-23 17:30:07 -0500

    Updated the general loop
    Need to be updated to include interrupt for button push and test outside

--- uno/Display/DISPLAY THAT WORKS/DISPLAY THAT WORKS.ino
+++ uno/Display/DISPLAY THAT WORKS/DISPLAY THAT WORKS.ino
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <U8x8lib.h>
+#include <SoftwareSerial.h>
 
 /* Constructor */
 U8X8_SSD1306_128X64_NONAME_4W_SW_SPI u8x8(/* clock=*/ 13, /* data=*/ 11, /* cs=*/ 8, /* dc=*/ A0, /* reset=*/ 9);
+SoftwareSerial Rangefinder(/*RX*/3, /*TX*/4);
 
 const int buttonPin = 2;
 int buttonState = 0;
@@ -12,25 +14,34 @@ void setup(void)
 {
   u8x8.begin();
 
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(buttonPin, INPUT_PULLUP);
   u8x8.setFont(u8x8_font_chroma48medium8_r);
-
   
-
+  Rangefinder.begin(115200);
 }
 
 void loop(void)
   {
     buttonState = digitalRead(buttonPin);
-  if(buttonState != HIGH) {
-    u8x8.drawString(1,3,"Button Pressed");
-    delay(200);
+    const unsigned char buf[] = {0xD, 0xA, 0x4F, 0x4E, 0xD, 0xA};
+    const unsigned char buf2[] = {0xD, 0xA, 0x4F, 0x46, 0x46, 0xD, 0xA};
 
-  }
-  else {
+     if(buttonState != HIGH) {
+      Rangefinder.write(buf, 6);
+      u8x8.drawString(1, 3, "Range Finder Active");
+
+      Serial.print("Data Read: "); 
+      for(int i = 0; i < 7; i++) {
+        Serial.print((char)Rangefinder.read());
+        Serial.print(" ");
+        delay(300);
+      }
+      Serial.println(); 
+      delay(500);
+     }
+
       u8x8.clearDisplay();
+      Rangefinder.write(buf2, 7);
+      return; 
   }
-
-  delay(1000); 
-}
