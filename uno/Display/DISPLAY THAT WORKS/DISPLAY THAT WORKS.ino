43a224b2e2df57014e1711669c9d503fc6578064
Ahmed Al Busaidi <36058005+TheOGRafiki@users.noreply.github.com>
2022-01-23 21:54:55 -0500

    Added Interupt and accounts for button debounce

--- uno/Display/DISPLAY THAT WORKS/DISPLAY THAT WORKS.ino
+++ uno/Display/DISPLAY THAT WORKS/DISPLAY THAT WORKS.ino
@@ -3,45 +3,57 @@
 #include <U8x8lib.h>
 #include <SoftwareSerial.h>
 
+/* Pin Declaration */ 
+const int buttonPin = 2;
+const int rangefinderTx = 4;
+const int rangefinderRx = 3;
 /* Constructor */
 U8X8_SSD1306_128X64_NONAME_4W_SW_SPI u8x8(/* clock=*/ 13, /* data=*/ 11, /* cs=*/ 8, /* dc=*/ A0, /* reset=*/ 9);
-SoftwareSerial Rangefinder(/*RX*/3, /*TX*/4);
+SoftwareSerial Rangefinder(/*RX*/rangefinderRx, /*TX*/rangefinderTx);
 
-const int buttonPin = 2;
-int buttonState = 0;
-/* u8x8.begin() is required and will sent the setup/init sequence to the display */
+volatile long lastDebounceTime = 0;
+volatile long debounceDelay = 50;
+
+volatile bool buttonState = false; /* False = Not Pressed  True = Pressed*/
+
 void setup(void)
 {
   u8x8.begin();
 
-  Serial.begin(115200);
   pinMode(buttonPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), range, RISING);
+
+  Serial.begin(115200);
   u8x8.setFont(u8x8_font_chroma48medium8_r);
   
   Rangefinder.begin(115200);
 }
 
-void loop(void)
-  {
-    buttonState = digitalRead(buttonPin);
-    const unsigned char buf[] = {0xD, 0xA, 0x4F, 0x4E, 0xD, 0xA};
-    const unsigned char buf2[] = {0xD, 0xA, 0x4F, 0x46, 0x46, 0xD, 0xA};
+void loop(void) { 
+  u8x8.drawString(1, 3, "Waiting For Button Press");
+  delay(1000);
+}
 
-     if(buttonState != HIGH) {
-      Rangefinder.write(buf, 6);
+void range() { 
+  if((millis() - lastDebounceTime) > debounceDelay) {
+      buttonState = !buttonState; 
+      const unsigned char startCommand[] = {0xD, 0xA, 0x4F, 0x4E, 0xD, 0xA};
+      const unsigned char stopCommand[] = {0xD, 0xA, 0x4F, 0x46, 0x46, 0xD, 0xA};
+
+      /* Start Command*/
+      Rangefinder.write(startCommand, 6);
       u8x8.drawString(1, 3, "Range Finder Active");
 
       Serial.print("Data Read: "); 
+
       for(int i = 0; i < 7; i++) {
         Serial.print((char)Rangefinder.read());
-        Serial.print(" ");
-        delay(300);
       }
       Serial.println(); 
-      delay(500);
-     }
 
-      u8x8.clearDisplay();
-      Rangefinder.write(buf2, 7);
-      return; 
+      // u8x8.drawString()
+      Rangefinder.write(stopCommand, 7);
+
+      lastDebounceTime = millis();
+    }
   }
